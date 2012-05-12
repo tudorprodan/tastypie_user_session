1adb0263aca8d1403d57f270f3b535815eb3abc9
Tudor Prodan <tudor.prodan@gmail.com>
2012-05-12 23:42:07 +0300

    Updated setup.py package name with dashes instead of underscores

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name="tastypie_user_session",
+    name="tastypie-user-session",
     version="0.2",
     packages=find_packages(),
     author="Tudor Prodan",
     author_email="tudor.prodan@gmail.com",
-    description="User sessions on top of Tastypie",
+    description="User sessions on top of Tastypie, plus Facebook auth",
+    long_description="",
     license="MIT",
     keywords="tastypie session authentication facebook",
     url="https://github.com/tudorprodan/tastypie_user_session/",
